07653becc3c1c2e3eeeb26562921e9542958ee89
Youngerjesus <yjh930919@gmail.com>
2020-08-16 03:30:09 +0900

    resize video

--- src/main/java/com/studyolle/infra/config/SecurityConfig.java
+++ src/main/java/com/studyolle/infra/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/", "/login", "/sign-up", "/check-email-token",
+                .mvcMatchers("/", "/login", "/sign-up", "/check-email-token","/index",
                         "/email-login", "/login-by-email", "/search/study").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated();
